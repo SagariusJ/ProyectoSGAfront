8145d6332c4f57ce5718f3e10f674ba1142258e2
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-06-17 00:18:22 -0400

    Cambios en imagenes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,12 +17,12 @@ export interface Producto {
 })
 export class HomeComponent {
   productos: Producto[] = [
-    { imagen: 'assets/img/producto1.jpg', titulo: 'Nombre del Producto1', descripcion: 'Descripción breve del producto.', precio: 8990 },
-    { imagen: 'assets/img/producto1.jpg', titulo: 'Nombre del Producto2', descripcion: 'Descripción breve del producto.', precio: 4990 },
-    { imagen: 'assets/img/producto1.jpg', titulo: 'Nombre del Producto3', descripcion: 'Descripción breve del producto.', precio: 5990 },
-    { imagen: 'assets/img/producto1.jpg', titulo: 'Nombre del Producto4', descripcion: 'Descripción breve del producto.', precio: 6990 },
-    { imagen: 'assets/img/producto1.jpg', titulo: 'Nombre del Producto5', descripcion: 'Descripción breve del producto.', precio: 8990 },
-    { imagen: 'assets/img/producto1.jpg', titulo: 'Nombre del Producto6', descripcion: 'Descripción breve del producto.', precio: 9990 }
+    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto1', descripcion: 'Descripción breve del producto.', precio: 8990 },
+    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto2', descripcion: 'Descripción breve del producto.', precio: 4990 },
+    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto3', descripcion: 'Descripción breve del producto.', precio: 5990 },
+    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto4', descripcion: 'Descripción breve del producto.', precio: 6990 },
+    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto5', descripcion: 'Descripción breve del producto.', precio: 8990 },
+    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto6', descripcion: 'Descripción breve del producto.', precio: 9990 }
   ];
 
   startIndex: number = 0;

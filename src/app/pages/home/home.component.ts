e0ea2d995567e76f2b5341330f2df276efe59bdb
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-06-21 18:47:33 -0400

    Cambios para intento de reparo de carrusel de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SliceProductsPipe } from '../../pipes/slice-products.pipe';
-
+import { NgForOf } from '@angular/common';
+import { SlicePipe } from '@angular/common';
 export interface Producto {
   imagen: string;
   titulo: string;
@@ -11,18 +12,18 @@ export interface Producto {
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SliceProductsPipe],
+  imports: [SliceProductsPipe, NgForOf, SlicePipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   productos: Producto[] = [
-    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto1', descripcion: 'Descripción breve del producto.', precio: 8990 },
-    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto2', descripcion: 'Descripción breve del producto.', precio: 4990 },
-    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto3', descripcion: 'Descripción breve del producto.', precio: 5990 },
-    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto4', descripcion: 'Descripción breve del producto.', precio: 6990 },
-    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto5', descripcion: 'Descripción breve del producto.', precio: 8990 },
-    { imagen: 'producto1.jpg', titulo: 'Nombre del Producto6', descripcion: 'Descripción breve del producto.', precio: 9990 }
+    { imagen: 'fondo.jpg', titulo: 'Nombre del Producto1', descripcion: 'Descripción breve del producto.', precio: 8990 },
+    { imagen: 'fondo.jpg', titulo: 'Nombre del Producto2', descripcion: 'Descripción breve del producto.', precio: 4990 },
+    { imagen: 'fondo.jpg', titulo: 'Nombre del Producto3', descripcion: 'Descripción breve del producto.', precio: 5990 },
+    { imagen: 'fondo.jpg', titulo: 'Nombre del Producto4', descripcion: 'Descripción breve del producto.', precio: 6990 },
+    { imagen: 'fondo.jpg', titulo: 'Nombre del Producto5', descripcion: 'Descripción breve del producto.', precio: 8990 },
+    { imagen: 'fondo.jpg', titulo: 'Nombre del Producto6', descripcion: 'Descripción breve del producto.', precio: 9990 }
   ];
 
   startIndex: number = 0;
